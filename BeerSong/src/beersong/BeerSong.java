69f91ab54706981bd692b55347c34f18be4c49f1
DartVeider198 <40816563+DartVeider198@users.noreply.github.com>
2019-10-25 11:37:53 +0500

    BeerSong
    with Error

---
+++ BeerSong/src/beersong/BeerSong.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beersong;
+
+/**
+ *
+ * @author nafikovti
+ */
+public class BeerSong {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int beerNum = 99;
+        String word = "бутылок (бутылки)";
+        
+        while (beerNum > 0) {
+        
+            if (beerNum == 1) {
+                word = "бутылка";
+            }
+
+            System.out.println(beerNum + " " + word + " пива на стене");
+            System.out.println(beerNum + " " + word + " пива.");
+            System.out.println("Возьми одну.");
+            System.out.println("Пусти по кругу.");
+            
+            beerNum = beerNum - 1;
+            if (beerNum > 0) {
+                System.out.println(beerNum + " " + word + " пива на стене");
+            } else {
+                System.out.println("Нет пива на стене");
+            }
+        }
+        
+    }
+    
+}
