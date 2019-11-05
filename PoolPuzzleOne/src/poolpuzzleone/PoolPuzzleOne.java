bbf275db677b72ad68927134cf111933ac8ded7f
DartVeider198 <40816563+DartVeider198@users.noreply.github.com>
2019-11-05 23:53:38 +0500

    Fuuu

--- PoolPuzzleOne/src/poolpuzzleone/PoolPuzzleOne.java
+++ PoolPuzzleOne/src/poolpuzzleone/PoolPuzzleOne.java
@@ -3,7 +3,27 @@ package poolpuzzleone;
 public class PoolPuzzleOne {
 
     public static void main(String[] args) {
-
+        int x = 0;
+        
+        while (x < 4){
+            
+            x = x - 1;
+            
+            if(x < 1){
+                System.out.print("a ");
+            }
+            
+            x = x + 1;
+            
+            if(x < 1){
+                System.out.print("noise");
+                x = x + 2;
+            }
+            if(x == 1){
+                
+            }
+            
+        }
     }
     
 }
